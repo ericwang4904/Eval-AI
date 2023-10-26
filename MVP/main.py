ad781610d2608aa5c7cceec424d672915761f61e
Eric Wang <eriwang@nuevaschool.org>
2023-10-26 09:34:40 -0700

    nice

--- MVP/main.py
+++ MVP/main.py
@@ -9,5 +9,3 @@ class EvalWriter:
         load_dotenv()
         self.REPLICATE_API_TOKEN = os.getenv('REPLICATE_API_TOKEN')
         self.sheets_api = SheetsAPI(scopes=['https://www.googleapis.com/auth/spreadsheets.readonly'])
-
-    def 
---
+++ steps.md
@@ -0,0 +1,5 @@
+create new venv
+`pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib`
+`pip install python-dotenv`
+
+everything else is pip install NAME
